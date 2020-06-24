a0fc687eb9dc0dfa2aa52145950458b57ac0e098
Dario Santos <dariobennaia@gmail.com>
2020-06-24 01:22:05 -0300

    Cadastro na aplicaçao

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
@@ -16,8 +19,11 @@ const schema = Yup.object().shape({
 });
 
 function SignUp() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
   }
 
   return (
@@ -29,7 +35,7 @@ function SignUp() {
         <Input type="email" name="email" placeholder="Seu e-mail" />
         <Input type="password" name="password" placeholder="Sua senha" />
 
-        <button type="submit">Criar conta</button>
+        <button type="submit">{loading ? 'Aguarde...' : 'Criar conta'}</button>
         <Link to="/">Já tenho login</Link>
       </Form>
     </>
