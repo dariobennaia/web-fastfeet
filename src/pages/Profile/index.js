512340a4309f721f58493a0937865e8f6ce5a671
Dario Santos <dariobennaia@gmail.com>
2020-06-24 12:45:34 -0300

    Pagina de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit() {}
+
+  return (
+    <Container>
+      <Form initialData={profile}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Nome Completo" />
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit" onClick={handleSubmit}>
+          Atualizar perfil
+        </button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
 
 export default Profile;
